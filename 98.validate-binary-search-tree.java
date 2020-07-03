48f2c98f80b9cc977e99bfdef36b276cd5ac2b3a
Xinpei Tan <31877314+widmonstertony@users.noreply.github.com>
2020-07-03 10:19:05 -0700

    Create 98.validate-binary-search-tree.java

---
+++ 98.validate-binary-search-tree.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=98 lang=java
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        // 用long而不是int因为数字会超过int的范围
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    // 确认root的值一定要比min大，一定要比max小，否则就不是valid
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return isValidBST(root.left, min, root.val) & isValidBST(root.right, root.val, max);
+    }
+}
+// @lc code=end
+
