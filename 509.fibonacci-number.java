ce1ce77e0c37b223463cbbcf60b06ad406c416ec
Xinpei Tan <t347100955@gmail.com>
2020-07-14 16:43:59 -0700

    Create 509.fibonacci-number.java

---
+++ 509.fibonacci-number.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=509 lang=java
+ *
+ * [509] Fibonacci Number
+ */
+
+// @lc code=start
+class Solution {
+    public int fib(int N) {
+        if (N <= 1) {
+            return N;
+        }
+        return fib(N - 1) + fib(N - 2);
+    }
+}
+// @lc code=end
+
