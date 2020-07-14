e69fe8d997d61c407ad718c63821f325ea36970d
Xinpei Tan <t347100955@gmail.com>
2020-07-13 23:36:00 -0700

    Create 404.sum-of-left-leaves.java

---
+++ 404.sum-of-left-leaves.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=404 lang=java
+ *
+ * [404] Sum of Left Leaves
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        // 如果当前节点的左子节点是一个叶子，则加上这个叶子的值
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+}
+// @lc code=end
+
