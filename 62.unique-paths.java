76237d7eac3c918c83c0f1e94a17dd1b218d603d
Xinpei Tan <t347100955@gmail.com>
2020-07-14 23:27:57 -0700

    Update 62.unique-paths.java

--- 62.unique-paths.java
+++ 62.unique-paths.java
@@ -8,17 +8,16 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        // 最右边和最底下只有一种方式到达终点
-        for (int i = 0; i < m; i++) {
-            dp[i][n - 1] = 1;
-        }
-        for (int j = 0; j < n; j++) {
-            dp[m - 1][j] = 1;
-        }
-        for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                // 当前i，j点总共到达终点的方式数 等于从下面走的方式数加上从右边走的方式数
-                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                // 最右边和最底下只有一种方式到达终点
+                if (i == m - 1 || j == n - 1) {
+                    dp[i][j] = 1;
+                }
+                else {
+                    // 当前i，j点总共到达终点的方式数 等于从下面走的方式数加上从右边走的方式数
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                }
             }
         }
         return dp[0][0];
