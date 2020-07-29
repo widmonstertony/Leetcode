d1d07ea1e23445bb30a294d53710db3b7a2a9e05
Xinpei Tan <t347100955@gmail.com>
2020-07-29 00:03:41 -0700

    Update 464.can-i-win.java

--- 464.can-i-win.java
+++ 464.can-i-win.java
@@ -18,32 +18,33 @@ class Solution {
         if (maxChoosableInteger / 2 < desiredTotal / (maxChoosableInteger + 1)) {
             return false;
         }
-        Map<Integer, Boolean> canWinMap = new HashMap<>();
-        return canIWinHelper(maxChoosableInteger, desiredTotal, 0, canWinMap);
+        Map<Integer, Boolean> canWinStateMap = new HashMap<>();
+        return canIWinHelper(maxChoosableInteger, desiredTotal, 0, canWinStateMap);
     }
-    private boolean canIWinHelper(int maxChoosableInteger, int desiredTotal, int usedNum, Map<Integer, Boolean> canWinMap) {
-        if (canWinMap.containsKey(usedNum)) {
-            return canWinMap.get(usedNum);
+    private boolean canIWinHelper(int maxChoosableInteger, int desiredTotal, int currState, Map<Integer, Boolean> canWinStateMap) {
+        // 如果之前已经运行过当前state的结果，直接返回结果
+        if (canWinStateMap.containsKey(currState)) {
+            return canWinStateMap.get(currState);
         }
         // 遍历所有数字，将该数字对应的 mask 算出来
         // 也就是把所有可以选的数字都试一遍
         for (int i = 0; i < maxChoosableInteger; i++) {
             // 使用一个整型数，按位来记录数组中的某个数字是否使用过
             // 因为maxChoosableInteger不会超过20，整型数有32位
-            int curr = (1 << i);
-            // 如果其和 usedNum 相与为0的话，说明该数字没有使用过
-            if ((curr & usedNum) == 0) {
+            int currNum = (1 << i);
+            // 如果其和 currState 相与为0的话，说明该数字没有使用过
+            if ((currNum & currState) == 0) {
                 // 如果此时的目标值小于等于当前数字，说明已经赢了
                 // 或者如果对手按照这个状况玩下去不能赢，也代表当前玩家赢了
                 if (desiredTotal <= i + 1 || 
-                    !canIWinHelper(maxChoosableInteger, desiredTotal - (i + 1), curr | usedNum, canWinMap)) {
-                    canWinMap.put(usedNum, true);
+                  !canIWinHelper(maxChoosableInteger, desiredTotal - (i + 1), currNum | currState, canWinStateMap)) {
+                    canWinStateMap.put(currState, true);
                     return true;
                 }
             }
         }
         // 遍历完所有数字，标记 false，并返回 false
-        canWinMap.put(usedNum, false);
+        canWinStateMap.put(currState, false);
         return false;
     }
 }
