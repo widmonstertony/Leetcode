53ced070fdcc6b58a634099e1b03a7c4dc041f2e
Xinpei Tan <t347100955@gmail.com>
2020-08-01 00:24:41 -0700

    Update 486.predict-the-winner.java

--- 486.predict-the-winner.java
+++ 486.predict-the-winner.java
@@ -8,15 +8,23 @@
 class Solution {
     public boolean PredictTheWinner(int[] nums) {
         int n = nums.length;
+        // dp[i][j] 代表从i到j开始玩的话，当前玩家可以获得的比对方玩家多的分数的最大值
         int[][] dp = new int[n][n];
+        // 如果只有一个数可以选，则当前玩家一定能获得当前分数，而对方获得0分
         for (int i = 0; i < n; i++) {
             dp[i][i] = nums[i];
         }
+        // len表示为当前所选的数组的长度
         for (int len = 1; len < n; len++) {
+            // i从0开始，j一直从当前长度直到数组最后一位
             for (int i = 0, j = len; j < n; i++, j++) {
+                // 当前i，j的最大值
+                // 如果选择i也就是开头那位，则减去对方在i之后到j的能获得比你多的分数的最大值，也就是dp[i + 1][j]
+                // 如果选择j也就是末尾那位，则减去对方在j之前到i的能获得比你多的分数的最大值，也就是dp[i][j - 1]
                 dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
             }
         }
+        // 最后确认从开始到结束，当前玩家能获得的比对方玩家多的分数大于0，也就代表获胜了
         return dp[0][n - 1] >= 0;
     }
     // public boolean PredictTheWinner(int[] nums) {
