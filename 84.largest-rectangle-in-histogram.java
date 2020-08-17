b697280a890f8f24fcf35a3a7fef346706cbed3c
Tony Tan <t347100955@gmail.com>
2020-08-16 23:21:27 -0700

    Update 84.largest-rectangle-in-histogram.java

--- 84.largest-rectangle-in-histogram.java
+++ 84.largest-rectangle-in-histogram.java
@@ -23,23 +23,22 @@ class Solution {
         newHeights[heights.length] = 0;
 
         for (int i = 0; i < heights.length + 1; i++) {
-            
             // 遇到栈顶元素高于当前元素时，就要取出栈顶元素处理，一直到没有比当前元素更高的
             // 因为取出的元素到当前数字i的位置，中间所有的长方形高度一定是比取出的元素高的
             // 也就是从下一个要取出的元素坐标之后到i坐标之前，当前取出的元素高度一定是最低的
             // （因为是递增栈，所以下一个要取出来的一定比现在取出来的低）
             // 这样的话，面积则就一定是取出的元素高度乘以（下一个要取出的元素坐标之后到i的距离）
-            while (!increSt.isEmpty() && newHeights[i] < newHeights[increSt.peek()]) {
-                int curr = increSt.pop();
+            while (!increSt.isEmpty() && newHeights[increSt.peek()] > newHeights[i]) {
+                int prevHeightStartIdx = increSt.pop();
                 int currArea;
                 // 如果这时候st空了，说明在i之前没有比现在取出来的元素更低的元素
                 // 这种时候，面积就是现在的元素高度 * 到i坐标的距离
                 if (increSt.isEmpty()) {
-                    currArea = newHeights[curr] * i;
+                    currArea = newHeights[prevHeightStartIdx] * i;
                 }
                 // 否则面积就是取出的元素高度乘以（下一个要取出的元素坐标之后到i的距离）
                 else {
-                    currArea = newHeights[curr] * (i - 1 - increSt.peek());
+                    currArea = newHeights[prevHeightStartIdx] * (i - 1 - increSt.peek());
                 }
                 res = Math.max(res, currArea);
             }
