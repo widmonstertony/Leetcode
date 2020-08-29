9fe57bfbb89733c7fc623639210d11a13d4993c0
Tony Tan <t347100955@gmail.com>
2020-08-28 22:35:01 -0700

    Create 40.combination-sum-ii.java

---
+++ 40.combination-sum-ii.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=40 lang=java
+ *
+ * [40] Combination Sum II
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> resList = new ArrayList<>();
+        Arrays.sort(candidates);
+        combinationSumHelper(0, candidates, target, resList, new ArrayList<Integer>());
+        return resList;
+    }
+    // 注意需要一个start标注当前的数字的坐标，因为不想从比当前数更小的数重新试，这样会有重复
+    private void combinationSumHelper(int start, int[] candidates, int target, List<List<Integer>> resList, List<Integer> currList) {
+        if (target <= 0) {
+            if (currList.size() > 0 && target == 0) {
+                resList.add(new ArrayList<>(currList));
+            }
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            // 如果当前不是一开始那位并且当前数字和之前的一样，跳过
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            int candidate = candidates[i];
+            currList.add(candidate);
+            // 从下一个位置开始尝试，避免重复
+            combinationSumHelper(i + 1, candidates, target - candidate, resList, currList);
+            currList.remove(currList.size() - 1);
+        }
+    }
+}
+// @lc code=end
+
