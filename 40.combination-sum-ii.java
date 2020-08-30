35854f00bc49ca4f083880f9a6283556a18e5bdb
Tony Tan <t347100955@gmail.com>
2020-08-29 20:58:42 -0700

    Update 40.combination-sum-ii.java

--- 40.combination-sum-ii.java
+++ 40.combination-sum-ii.java
@@ -21,13 +21,14 @@ class Solution {
             return;
         }
         for (int i = start; i < candidates.length; i++) {
-            // 如果当前不是一开始那位并且当前数字和之前的一样，跳过
+            // 如果当前数字不是一开始那位（避免一个位置上的数被多次使用
+            // 并且如果当前数字和之前的一样，则跳过当前数字(避免同一种数字被多次使用)
             if (i > start && candidates[i] == candidates[i - 1]) {
                 continue;
             }
             int candidate = candidates[i];
             currList.add(candidate);
-            // 从下一个位置开始尝试，避免重复
+            // 从下一个新的位置开始尝试，避免重复使用当前这个位置上的数字
             combinationSumHelper(i + 1, candidates, target - candidate, resList, currList);
             currList.remove(currList.size() - 1);
         }
