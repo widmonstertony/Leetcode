1ea0838b45ee34ed2759484765ac61a30b2fe8f0
Tony Tan <t347100955@gmail.com>
2020-09-01 21:45:21 -0700

    Create 526.beautiful-arrangement.java

---
+++ 526.beautiful-arrangement.java
@@ -0,0 +1,42 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+/*
+ * @lc app=leetcode id=526 lang=java
+ *
+ * [526] Beautiful Arrangement
+ */
+
+// @lc code=start
+class Solution {
+    public int countArrangement(int N) {
+        // 保存所有答案，可以用个count，这里为了debug方便用了List
+        List<List<Integer>> resList = new LinkedList<>();
+        boolean[] visited = new boolean[N];
+        // 可以用一个数来代替这个list用来表示当前尝试到第几位
+        // 这里用它的size来知道遍历到了第几位
+        List<Integer> currList = new LinkedList<>();
+        helper(currList, visited, resList);
+        return resList.size();
+    }
+    private void helper(List<Integer> currList, boolean[] visited, List<List<Integer>> resList) {
+        if (currList.size() == visited.length) {
+            resList.add(new LinkedList<>(currList));
+            return;
+        }
+        for (int i = 1; i <= visited.length; i++) {
+            if (visited[i - 1]) {
+                continue;
+            }
+            if (i % (currList.size() + 1) == 0 || (currList.size() + 1) % i == 0) {
+                visited[i - 1] = true;
+                currList.add(i);
+                helper(currList, visited, resList);
+                currList.remove(currList.size() - 1);
+                visited[i - 1] = false;
+            }
+        }
+    }
+}
+// @lc code=end
+
