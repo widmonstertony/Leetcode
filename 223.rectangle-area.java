d8e1e8ce476c82fec0733f4db2fa4f5ca0000c04
Tony Tan <t347100955@gmail.com>
2020-09-13 15:47:12 -0700

    Update 223.rectangle-area.java

--- 223.rectangle-area.java
+++ 223.rectangle-area.java
@@ -7,7 +7,10 @@
 // @lc code=start
 class Solution {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        //先求出重叠区间的四个点 topLeft，topRight，bottomLeft，bottomRight的值
+        // 先求出重叠区间的四个点 topLeft，topRight，bottomLeft，bottomRight的值
+        // 由于交集都是在中间
+        // 横边的左端点是两个矩形左顶点横坐标的较大值, 右端点是两个矩形右顶点的较小值
+        // 竖边的下端点是两个矩形下顶点纵坐标的较大值，上端点是两个矩形上顶点纵坐标的较小值
         int topLeft = Math.max(A, E), topRight = Math.max(Math.min(C, G), topLeft);
         int bottomLeft = Math.max(B, F), bottomRight = Math.max(Math.min(D, H), bottomLeft);
         // 再求出重叠区间的面积, 两个长方形面积之和减去重叠面积就是剩余面积
