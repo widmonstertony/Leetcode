b5b507f08a6373deaa9c1429e0ad41787642f735
Tony Tan <t347100955@gmail.com>
2020-09-29 22:48:48 -0700

    Created 623

---
+++ 669.trim-a-binary-search-tree.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=669 lang=java
+ *
+ * [669] Trim a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return root;
+        }
+        // 如果当前root的值比最小值都还小，说明只能要root的右半部分
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+}
+// @lc code=end
+
