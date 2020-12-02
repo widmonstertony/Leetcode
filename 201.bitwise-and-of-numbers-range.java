6a11d9f9a44f15dd7977bffe807a5194cc64e7d7
Tony Tan <t347100955@gmail.com>
2020-12-01 20:47:03 -0800

    Update 201.bitwise-and-of-numbers-range.java

--- 201.bitwise-and-of-numbers-range.java
+++ 201.bitwise-and-of-numbers-range.java
@@ -8,7 +8,13 @@
 class Solution {
     public int rangeBitwiseAnd(int m, int n) {
         // 最后的数是该数字范围内所有的数的左边共同的部分
-        // 需要找到m和n公共的左边部分
+        // m <= n so,
+        // m = xxxx0____
+        // n = xxxx1____ 因为 
+        // xxxx01.111 >= m
+        // xxxx10.000 <= n 
+        // xxxx01.111, xxxx10.000 都是在m和n之间的数, 他们的AND结果是0
+        // 所以我们只需要找到m和n公共的左边部分
         // 先创建一个32位都是1的mask
         int mask = Integer.MAX_VALUE;
         // 一直左移，直到找到m和n的公共部分
