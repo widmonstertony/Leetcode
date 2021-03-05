ecb588e9fc87bbe7ad39c3853afcd92020ebd687
Xinpei Tan <t347100955@gmail.com>
2021-03-04 18:05:30 -0800

    Create 265.paint-house-ii.java

---
+++ 265.paint-house-ii.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=265 lang=java
+ *
+ * [265] Paint House II
+ */
+
+// @lc code=start
+class Solution {
+    public int minCostII(int[][] costs) {
+        if (costs.length == 0 || costs[0].length == 0) {
+            return 0;
+        }
+        // 因为这个只用到了上一个位置的信息，所以可以优化成一维数组
+        int[][] minCostDp = new int[costs.length][costs[0].length];
+        for (int i = 0; i < costs.length; i++) {
+            for (int j = 0; j < costs[i].length; j++) {
+                if (i == 0) {
+                    minCostDp[i][j] = costs[i][j];
+                }
+                // 当前i位置选j颜色的最小cost，等于上一个位置不选j的最小cost+当前位置j的cost
+                else {
+                    minCostDp[i][j] = Integer.MAX_VALUE;
+                    for (int k = 1; k < costs[i].length; k++) {
+                        minCostDp[i][j] = Math.min(minCostDp[i][j], minCostDp[i - 1][(j + k) % costs[i].length] + costs[i][j]);
+                    }
+                }
+            }
+        }
+        // 找出最后一个房子的所有颜色里的最小花费
+        int res = Integer.MAX_VALUE;
+        for (int j = 0; j < costs[0].length; j++) {
+            res = Math.min(res, minCostDp[costs.length - 1][j]);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
